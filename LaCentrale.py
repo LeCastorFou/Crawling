82fbd764cd438e72b70bbc3652abd05b21fdbbb6
valentin <valentin.lefranc@see-d.fr>
2018-06-13 17:28:36 +0200

    new

--- LaCentrale.py
+++ LaCentrale.py
@@ -3,9 +3,6 @@ import urllib3
 import bs4 as BeautifulSoup
 import numpy as np
 import pandas as pd
-from unidecode import unidecode
-import encodings
-import codecs
 import re
 import unicodedata
 
@@ -33,20 +30,22 @@ import unicodedata
 http = urllib3.PoolManager()
 
 T = 16
-page = 1
 
-while (T == 16):
+page = 1329
+pageInit = page
+
+while (T > 0):
     print('Page numer0 : ' + str(page) + ' Nb annonces ' + str(T))
-    if (page == 1) :
-        url = 'https://www.lacentrale.fr/listing?makesModelsCommercialNames=CITROEN&page=1'
+    if (page == pageInit) :
+        url = 'https://www.lacentrale.fr/listing?makesModelsCommercialNames=CITROEN&page=1329'
         #print(url)
     else:
         ptr = 'page='+str(page)
         #print(ptr)
-        url2 = url.replace("page=1",ptr)
+        url2 = url.replace("page=1329",ptr)
         #print(url2)
 
-    if(page == 1):
+    if(page == pageInit):
         response = http.request('GET', url)
     else:
         response = http.request('GET', url2)
@@ -55,9 +54,9 @@ while (T == 16):
     soup = BeautifulSoup.BeautifulSoup(response.data)
 
     allcont = soup.findAll('div',class_ = "adContainer ")
-    #dir(allcont[0])
-    T =len(allcont)
-    Title = np.array(['mode','version','prix','km','annee'])
+    T = len(allcont)
+
+    Title = np.array(['mode','version','prix','km','annee','part','dpt','Gar'])
 
     for i in range(0,T):
         model = allcont[i].span.text
@@ -71,19 +70,55 @@ while (T == 16):
         km = unicodedata.normalize('NFKD',km).encode('ascii','ignore')
         km = str(km)
         km = re.sub("[^0-9]", "", km)
+
+        # Pas toujour le meme format entre garantie et particulier
+        p = 2
+        g = 4
+        #print(len(allcont[i].findAll('p')))
+        #for j in range(0,len(allcont[i].findAll('p')) ):
+            #print(allcont[i].findAll('p')[j] )
+
+        if len(allcont[i].findAll('p')) == 3:
+            p = 1
+            g = 2
+        if len(allcont[i].findAll('p')) == 4:
+            p = 2
+            g = 3
+        if len(allcont[i].findAll('p')) == 2:
+            p = 0
+            g = 1
+        # code particulier
+        particulier = 0
+        if allcont[i].findAll('p')[p].text == "Particulier":
+            particulier = 1
+        #departement
+        dpt = allcont[i].find('div',class_ = "dptCont bold txtGrey7C lH35").text
+        dpt = dpt.replace('Dpt. ','')
+
+        # garantie en mois
+        Gar = allcont[i].findAll('p')[g].text
+        Gar = unicodedata.normalize('NFKD',Gar).encode('ascii','ignore')
+        Gar= str(Gar)
+        Gar =  re.sub("[^0-9]", "", Gar)
+
         if i == 0:
-            annonce = np.array([model,version,prix,km,year])
+            annonce = np.array([model,version,prix,km,year,particulier,dpt,Gar])
         else:
-            annonce = np.vstack((annonce,np.array([model,version,prix,km,year]) ))
+            annonce = np.vstack((annonce,np.array([model,version,prix,km,year,particulier,dpt,Gar]) ))
     annonce = pd.DataFrame(annonce)
     annonce.columns = Title
 
-    if(page == 1):
+    if(page == pageInit):
         Data = annonce
     else:
         Data = Data.append(annonce)
 
     page = page +1
+
+    if page > 1999:
+        break
+    #if page == 4:
+    #    T=1
 
 
 Data.to_csv("CrawlerCitroen.csv", sep=',')
--- PricingCitroen.py
+++ PricingCitroen.py
@@ -12,9 +12,11 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 data = pd.read_csv("CrawlerCitroen.csv")
-data['mode'].unique()
-len(data['version'].unique())
-
+#data = pd.read_csv("Q:\ProjetsInternes\PricingVO\donnees\CrawlerCitroen.csv",encoding="utf-8")
+#data['mode'].unique()
+#len(data['version'].unique())
+data = data.drop(data.columns[0], axis=1)
+data = data.drop(data.columns[0], axis=1)
 
 data_sub = data[data['mode']=="CITROEN DS4"]
 plt.plot(data_sub['prix'],data_sub['km'],'ro')
@@ -25,22 +27,14 @@ plt.show()
 
 plotly.tools.set_credentials_file(username='valentin.lefranc', api_key='Xb0HU4LGnX8h3COhUgJr')
 
-trace1 = go.Scatter(
-    x = data_sub['prix'],
-    y = data_sub['km'],
-    mode='markers',
-    marker=dict(size='16',color = data_sub['km'],showscale=True))
-data = [trace1]
-
-
 len(data_sub)
-
-data_subtrain = data_sub[1:500]
-data_subtest = data_sub[501:540]
+data_sub.columns
+data_subtrain = data_sub[1:800]
+data_subtest = data_sub[801:1032]
 
 
 ############# Random RandomForest
-features = data_subtrain.columns[[4,5]]
+features = data_subtrain.columns[[3,4,5,7]]
 y = data_subtrain['prix']
 clf = RandomForestRegressor(n_estimators=20)
 clf.fit(data_subtrain[features], y)
@@ -106,3 +100,153 @@ plt.show()
 # dispersion du prix de la ds4
 hist = [go.Histogram(x=dataVds4['prix'])]
 py.iplot(hist, filename='basic histogram')
+
+
+# On cherche le model le plus vendu avec le moteur le plus récurent
+Modver = data.groupby(['mode','version']).apply(len)
+Modver = pd.DataFrame(Modver)
+Modver.columns = np.array(['no'])
+Modver = Modver.sort_values(by =['no'], ascending=False)
+
+Modver
+
+
+data_max = data[data['mode']=="CITROEN C3 (3E GENERATION)"]
+data_max = data_max[data_max['version']=="III 1.2 PURETECH 82 FEEL"]
+len(data_max)
+data_max
+
+plt.plot(data_max['prix'],data_max['km'],'ro')
+plt.show()
+
+sns.lmplot('prix','km', data=data_max, hue='version', fit_reg=False)
+plt.show()
+
+## Mot clef pour les versions de C3
+C3ver = np.array(data_max['version'])
+keyw = C3ver[1].split(" ")
+keyw[0]
+len(C3ver)
+for i in range(0,len(C3ver)):
+    if i == 0:
+        keyw = C3ver[i].split(" ")
+    if i > 0:
+        keyw = np.append(keyw,C3ver[i].split(" "))
+
+np.unique(keyw)
+
+## Mot clef pour les versions citroen
+Nver = np.array(data['version'])
+Allkeyw = Nver[1].split(" ")
+
+for i in range(0,len(Nver)):
+    if i == 0:
+        Allkeyw = Nver[i].split(" ")
+    if i > 0:
+        Allkeyw = np.append(Allkeyw,Nver[i].split(" "))
+
+
+
+Allkeyw = pd.DataFrame(Allkeyw)
+Allkeyw.columns = np.array(['kw'])
+Allkeyw_S = Allkeyw.groupby(['kw']).apply(len)
+Allkeyw_S = pd.DataFrame(Allkeyw_S)
+Allkeyw_S.columns = np.array(['oc'])
+Allkeyw_S = Allkeyw_S.sort_values(by =['oc'], ascending=False)
+# On ne garde que les mots clefs qui apparaissent plus d'une fois sur 100
+Allkeyw_S = Allkeyw_S[Allkeyw_S['oc'] > 0.01*len(Allkeyw)]
+Allkeyw_S.index[2]
+
+data['kw'] = 1
+a = np.linspace(0,0,len(Allkeyw_S))
+
+for j in range(0,len(Allkeyw_S)):
+    for i in range(0,len(data['version'])):
+         if np.array(data['version'])[i].find(Allkeyw_S.index[j])   == -1:
+             b=1
+         if np.array(data['version'])[i].find(Allkeyw_S.index[j])   != -1:
+            a[j] = a[j] + np.array(data['version'])[i].find(Allkeyw_S.index[j])
+
+    a[j] = a[j]/np.array(Allkeyw_S)[j]
+
+
+kwdf = pd.DataFrame(np.array(Allkeyw_S.index))
+kwdf['pos'] = a
+kwdf = kwdf.sort_values(by =['pos'], ascending=True)
+kwdf['type'] = "Uk"
+kwdf['type'][kwdf['pos']<1] = "Nmod"
+kwdf['type'][kwdf['pos']>3 ] = "litre"
+kwdf['type'][kwdf['pos']>5 ] = "moteur"
+kwdf['type'][kwdf['pos']>10] = "PUI"
+kwdf['type'][kwdf['pos']>17] = "OPT"
+
+data['litre'] = 1
+litre = kwdf[kwdf['type'] == "litre"]
+litre.columns = np.array(['litre','pos','type'])
+
+for j in range(0,len(litre)):
+    print(np.array(litre['litre'])[j])
+    for i in range(0,len(data['version'])):
+        if np.array(data['version'])[i].find( np.array(litre['litre'])[j] )   != -1:
+             data['litre'][data.index[i]] = np.array(litre['litre'])[j]
+
+data['moteur'] = 'moteur'
+moteur = kwdf[kwdf['type'] == "moteur"]
+moteur.columns = np.array(['moteur','pos','type'])
+moteur
+
+for j in range(0,len(moteur)):
+    print(np.array(moteur['moteur'])[j])
+    for i in range(0,len(data['version'])):
+        if np.array(data['version'])[i].find( np.array(moteur['moteur'])[j] )   != -1:
+             data['moteur'][data.index[i]] = np.array(moteur['moteur'])[j]
+
+data_f = data
+data_f = data_f.replace({'E-HDI': 1}, regex=True)
+data_f = data_f.replace({'BLUEHDI': 2}, regex=True)
+data_f = data_f.replace({'PURETECH': 3}, regex=True)
+data_f = data_f.replace({'moteur': 0}, regex=True)
+
+data_f
+
+
+data_sub = data_f[data_f['mode']=="CITROEN DS4"]
+len(data_sub)
+data_sub.columns
+
+data_subtrain = data_sub[1:800]
+data_subtest = data_sub[801:1032]
+
+############# Random RandomForest
+features = data_subtrain.columns[[3,4,5,7,9,10]]
+y = data_subtrain['prix']
+clf = RandomForestRegressor(n_estimators=20)
+clf.fit(data_subtrain[features], y)
+y_test = data_subtest['prix']
+
+data_subtest['prediction'] = clf.predict(data_subtest[features])
+data_subtest['error'] = (data_subtest['prix'] - clf.predict(data_subtest[features]))/data_subtest['prix']
+hist = [go.Histogram(x=data_subtest['error'])]
+py.iplot(hist, filename='basic histogram')
+
+Emoy = np.sum( abs(data_subtest['error']) )/len(data_subtest.index)
+Emoy
+
+############# Random RandomForest c3 PURETECH
+data_max.columns
+data_subtrain = data_max[1:400]
+data_subtest = data_max[401:574]
+
+features = data_subtrain.columns[[3,4,5,7]]
+y = data_subtrain['prix']
+clf = RandomForestRegressor(n_estimators=20)
+clf.fit(data_subtrain[features], y)
+y_test = data_subtest['prix']
+
+data_subtest['prediction'] = clf.predict(data_subtest[features])
+data_subtest['error'] = (data_subtest['prix'] - clf.predict(data_subtest[features]))/data_subtest['prix']
+hist = [go.Histogram(x=data_subtest['error'])]
+py.iplot(hist, filename='basic histogram')
+
+Emoy = np.sum( abs(data_subtest['error']) )/len(data_subtest.index)
+Emoy
